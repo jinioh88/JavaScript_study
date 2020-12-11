54176062bdb4f2ae636d6f8e4568943fd91729af
jinioh88 <jinioh88@gmail.com>
2020-12-11 23:29:32 +0900

    기본 타입

--- vanila/index.js
+++ vanila/index.js
@@ -1,1 +1,9 @@
-console.log('Im Working. Im JS. Im Buery.')
+var str = 'test';
+console.log(str[0]);
+
+str[0] = 'T';
+console.log(str);
+
+var nullVar = null;
+console.log(typeof nullVar);
+console.log(nullVar === null);d            
