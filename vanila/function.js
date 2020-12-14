314c96a69e6648e4ccde961aad82f03ad3c398bc
jinioh88 <jinioh88@gmail.com>
2020-12-15 00:40:08 +0900

    this

--- vanila/function.js
+++ vanila/function.js
@@ -1,12 +1,11 @@
-// 함수 리터럴
-function add(x, y) {
-    return x+y;
+function parent() {
+    var a = 100;
+    var child = function() {
+        console.log(a);
+    }
+
+    return child;
 }
 
-// 기명 함수 표현식 주의점
-const add = function sum(x, y) {
-    return x+y;
-}
-
-console.log(add(3,4)); // 가능
-console.log(sum(4, 5)); // 불가능
+var inner = parent();
+inner();
--- vanila/index.js
+++ vanila/index.js
@@ -1,10 +1,24 @@
-var p = function Person(name) {
-    this.name = name;
-}
+var value = 100;
 
-p.sayHi = function () {
-    return 'Hi my name is $(this.name)';
-}
+var myObject = {
+    value: 1, 
+    func1: function() {
+        var that = this;
+        this.value += 1;
+        console.log('func1() called. this.value : ' + this.value);
 
-const me = new p('oh');
-console.log(me.constructor.sayHi());
+        func2 = function() {
+            that.value += 1;
+            console.log('func2() called. this.value : ' + that.value);
+            
+            func3 = function() {
+                that.value += 1;
+                console.log('func3() called. this.value : ' + that.value);
+            }
+            func3();
+        }
+        func2();
+    }
+};
+
+myObject.func1();
