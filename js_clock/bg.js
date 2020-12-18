c1b163be83c9e5e3b1d294ab14317d7f27b814d9
jinioh88 <jinioh88@gmail.com>
2020-12-18 18:42:44 +0900

    배경화면 추가

---
+++ js_clock/bg.js
@@ -0,0 +1,21 @@
+const body = document.querySelector("body");
+const IMG_NUMBER = 3;
+
+function paintImage(imgNumber) {
+    const image = new Image();
+    image.src = `image/${imgNumber + 1}.jpg`;
+    image.classList.add("bgImage");
+    body.prepend(image);
+}
+
+function genReandom() {
+    const number = Math.floor(Math.random() * IMG_NUMBER);
+    return number;
+}
+
+function init() {
+    const randomNumber = genReandom();
+    paintImage(randomNumber);
+}
+
+init();
