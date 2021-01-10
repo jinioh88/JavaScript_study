a1c8afa188f1a9a91d0ad834818959e607f7d509
jinioh88 <jinioh88@gmail.com>
2021-01-10 10:26:57 +0900

    객체리터럴

--- deep-dive/ch03/index.js
+++ deep-dive/ch03/index.js
@@ -2,3 +2,10 @@ console.log(score);
 score = 80;
 var score;
 console.log(score);
+
+var first = 'OH';
+var last = 'SEJIN';
+console.log(`My name is ${first} ${last}`);
+
+var key = Symbol('key');
+console.log(typeof key);
---
+++ deep-dive/ch09/ex1.js
@@ -0,0 +1,58 @@
+var done = true;
+var message = '';
+
+if (done) message = '완료';
+else message = '미완료';
+
+message = done ? '완료' : '미완료';
+console.log(message);
+
+var elem = null;
+var value = elem && elem.value;
+
+// 단축 평가를 사용한 매개변수의 기본값 설정
+function getStringLength(str) {
+    str = str || '';
+    return str.length;
+}
+
+getStringLength();  // 0
+
+// ES6의 매개변수의 기본값 설정
+function getStringLen(str = '') {
+    return str.length;
+}
+
+var elem = null;
+var value = elem?.value;
+console.log(value);
+
+var str = '';
+
+var length = str && str.length;  // ''
+var len2 = str?.length; // 0
+
+var foo = '' || 'default string'; // ''
+var foo2 = '' ?? 'default string'; // default string
+
+var person = {
+    firstName: 'OH'
+};
+
+person.age = 30;
+console.log(person);
+
+var obj = {};
+var key = 'hello';
+
+obj[key] = 'world'; // es5 방식
+var obj = { [key]: 'world' }; // es6 방식
+
+// ES5
+var obj = {
+    x: x,
+    y: y
+};
+
+// ES6 
+var obj = { x, y }
