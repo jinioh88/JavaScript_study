66f439e3d0955d47c81b2e27a80611826fd6d0e1
jinioh88 <jinioh88@gmail.com>
2021-01-20 23:56:54 +0900

    프로퍼티 어트리뷰트

---
+++ deep-dive/ch16/ex.js
@@ -0,0 +1,33 @@
+const o = {};
+// o.[[Prototype]]; // error
+o.__proto__;    // Object.prototype
+
+const person = {
+    firstName: 'SEJIN',
+    lastName: 'OH',
+
+    //getter
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    },
+    // setter
+    set fullName(name) {
+        [this.firstName, this.lastName] = name.split(' ');
+    }
+};
+
+// setter 호출
+person.fullName = 'JINI OH';
+// getter 호출
+console.log(person.fullName);
+
+const p = {};
+Object.defineProperty(person, 'firstName', {
+    value: 'SEJIN',
+    writable: true,
+    enumerable: true,
+    configurable: true
+});
+Object.defineProperty(person, 'lastName', {
+    value: 'OH'
+});
