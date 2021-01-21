16c44c5c55cd328042be2ada636c853791186387
OH Nick <nick@herren.co.kr>
2021-01-21 20:21:24 +0900

    생성자 함수

--- deep-dive/ch17/ex.js
+++ deep-dive/ch17/ex.js
@@ -6,3 +6,33 @@ person.sayHello = function () {
 
 console.log(person);
 person.sayHello();
+
+function Circle(radius) {
+    if (!new.target) {
+        return new Circle(radius);
+    }
+
+    this.radius = radius;
+    this.getDiameter = function () {
+        return 2*this.radius;
+    };
+}
+
+const circle1 = new Circle(5);
+
+// 함수는 다양한 방식으로 호출될 수 있다. 
+function foo() {
+    console.log(this);
+}
+
+// 일반적 함수 호출
+foo();
+
+const obj = { foo };    // ES6 프로퍼티 축약 표현
+
+// 메서드로서 호출
+obj.foo();
+
+// 생성자 함수로서 호출
+const inst = new foo();
+
