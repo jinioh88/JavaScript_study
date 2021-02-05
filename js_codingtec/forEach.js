621fd3a2c8ebb1fe95b12401681af41de4578761
OH Nick <nick@herren.co.kr>
2021-02-05 11:26:56 +0900

    forEach

---
+++ js_codingtec/forEach.js
@@ -0,0 +1,11 @@
+const sailingClub = [
+    'yi hong',
+    'andy',
+    'darcy',
+    'jessi',
+    'alex',
+    'nathan',
+];
+
+const captialized = sailingClub.forEach(name => name.toUpperCase()); 
+console.log(captialized); // undefined
